5cbf2f19a98043c72d9a7ead6740429f11b88011
robinqc <robinquinteroc@gmail.com>
2025-09-15 05:11:50 -0500

    add support formobile searchbar

--- src/components/Header/logo.tsx
+++ src/components/Header/logo.tsx
@@ -3,7 +3,7 @@ export default function Logo() {
     return (
         <div className="flex items-center space-x-2 cursor-pointer">
               <Home className="h-5 w-5 text-primary-background" />
-            <span className="text-xl font-bold text-foreground font-[Cinzel]">MILLION</span>
+            <span className="text-xl font-bold text-foreground font-[Cinzel]">PROPERTY</span>
           </div>   
     )
 }
--- src/components/Header/right-buttons.tsx
+++ src/components/Header/right-buttons.tsx
@@ -1,15 +1,30 @@
-import { User, Menu } from "lucide-react"
+"use client"
+import { User, Menu, X } from "lucide-react"
 import { Button } from "@/components/ui/button"
+import { useState } from "react";
 
-export default function RightButtons() {
+interface Props {
+    isMobileSearchOpen: boolean;
+    setIsMobileSearchOpen: (isOpen: boolean) => void;
+}
+
+export default function RightButtons({
+    isMobileSearchOpen,
+    setIsMobileSearchOpen
+}: Props) {
     return (
         <div className="flex items-center space-x-4">
 
             <Button variant="ghost" size="icon">
             </Button>
 
-            <Button variant="ghost" size="icon" className="md:hidden">
-              <Menu className="h-5 w-5" />
+            <Button
+              variant="ghost"
+              size="icon"
+              className={`md:hidden`}
+              onClick={() => setIsMobileSearchOpen(!isMobileSearchOpen)}
+            >
+              {isMobileSearchOpen ? <X className="h-5 w-5" /> : <Menu className="h-5 w-5" />}
             </Button>
           </div>
     )
