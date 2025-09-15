3261cc1eb1d0471f357498a076f9481c16d26910
robinqc <robinquinteroc@gmail.com>
2025-09-15 12:22:32 -0500

    remove alias

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 import type { NextConfig } from "next";
 
-import path from "path";
-
 const nextConfig: NextConfig = {
-  webpack(config) {
-    config.resolve.alias["@"] = path.resolve(__dirname, "src");
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
